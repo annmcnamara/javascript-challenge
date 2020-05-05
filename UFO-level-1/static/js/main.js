72d5aa87c8bb28f802408a79434e17a767f480a9
annmcnamara@gmail.com <annmcnamara@gmail.com>
2020-05-05 16:08:57 -0500

    getting there...

--- UFO-level-1/static/js/main.js
+++ UFO-level-1/static/js/main.js
@@ -15,11 +15,15 @@
 //     // });
 // });
 
-function createTable(tableData) {
-    var table     = document.createElement('table');
-    var tableBody = document.createElement('tbody');
 
-    table.style.border = '2px solid black'
+function createTable(tableData) {
+    // var table     = document.createElement('table');
+    // var tableBody = document.createElement('tbody');
+
+    var table = document.querySelector("table-area");
+    var tableBody = document.querySelector("tbody");
+
+    //table.style.border = '2px solid black'
   
     tableData.forEach(function(rowData) {
       var row = document.createElement('tr');
@@ -33,6 +37,25 @@ function createTable(tableData) {
       Object.entries(rowData).forEach(([key, value])=>{
         var cell = document.createElement('td');
         if(key === "datetime"){ console.log(typeof value); }
+        if(key === "comments"){
+            value = value.replace(/\&\#/g,"");
+            value = value.replace(/44/g," ");
+            value = value.replace(/33/g," ");
+            value = value.replace(/39/g, " ")
+
+        }
+
+        if(key==='state'){
+            value = value.toUpperCase();
+        }
+
+        if(key==='country'){
+            value = value.toUpperCase();
+        }
+
+        if(key=='city'){
+            value = value.charAt(0).toUpperCase() + value.slice(1);
+        }
         // theValue = value.replace('a', 'b');
         cell.appendChild(document.createTextNode(value));
         row.appendChild(cell);
@@ -45,35 +68,30 @@ function createTable(tableData) {
     document.body.appendChild(table);
   }
   
+//createTable(data);
 createTable(data);
-
 function selectDate(sighting) {
-    var x = document.getElementById("myDate");
-    var searchValue = x.value;
-    
-    document.getElementById("demo").innerHTML = `Search Date is  ${x.value}`;
-    ddmmyyyy = sighting.datetime.split("/");
-    day    = ddmmyyyy[0];
-    month  = ddmmyyyy[1];
-    year   = ddmmyyyy[2];
-
-    if(day < 10){
-        day = "0"+day;
-    }
-
-    if(month < 10){
-        month = "0"+month;
-    }
-
-    theDate = year+"-"+month+"-"+day;
-    console.log(theDate)
-    console.log(searchValue)
-
-    return(theDate == searchValue)
-
+    var x = document.getElementById("datetime")
+    //console.log(`${x.value} compared to ${sighting.datetime}`);
+    return(x.value == sighting.datetime)
 }
 
-var dateSearched = data.filter(selectDate);
+function printTable(results){
+    Object.entries(results).forEach(([key, value])=>{
+        console.log(value);
+    });
+}
 
-console.log(`___________________`)
-console.log(dateSearched)
+// document.getElementById("dateInput").addEventListener("change", function() {
+//     var input = this.value;
+//     var dateEntered = new Date(input);
+//     console.log(input); //e.g. 2015-11-13
+//     console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
+// });
+
+function onClick (){
+    var results = data.filter(selectDate);
+    //clearTable();
+    createTable(results)
+    console.log(results)
+}
