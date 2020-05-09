d19c89504035dde739ca7d93a5a369807312130e
annmcnamara@gmail.com <annmcnamara@gmail.com>
2020-05-09 08:12:54 -0500

    Why cant I press return

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,6 +2,8 @@ var filterValue;
 var filterName;
 
 var text = d3.selectAll("#text");
+var button = d3.select("#filter-btn");
+
 //var text = d3.selectAll("#form-control>name")
 
 function cleanUpValue(key, value){
@@ -40,119 +42,39 @@ function createTable(tableData) {
     });
 }
   
-//createTable(data);
-
-function filterTable(sighting) {
-    //var x = document.getElementByName("datetime");
-    var inputElement = d3.select("#text");
-    var inputElement = d3.selectAll(".form-control")
-
-    // Get the value property of the input element
-    console.log(inputElement.keys)
-    var inputValue = inputElement.property("value");
-    var inputName  = inputElement.property("name");
-
-    console.log(inputValue);
-    console.log(inputName);
-    //console.log(`${inputValue} compared to ${sighting.datetime}`);
-
-    return(inputValue === sighting.datetime)
-}
-
-
-function filterDate(sighting){
-    var inputElement = d3.selectAll(".form-control")
-
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-    var inputName  = inputElement.property("name");
-
-    // console.log(inputValue);
-    // console.log(inputName);
-    //console.log(`${inputValue} compared to ${sighting.datetime}`);
-    //console.log("Filtering Date");
-
-    return(inputValue === sighting.datetime)
-
-}
-
-function filterCity(sighting){
-    console.log(`${filterValue} compared to ${sighting.city}`);
-    return(filterValue === sighting.city)
-}
-
+createTable(data);
 
 function filterData(sighting){
     console.log(`${filterName}: ${filterValue} compared to ${sighting.city}`);
+    filtered = data;
     switch(filterName) {
         case "datetime":
           // code block
           console.log(`${filterValue} compared to ${sighting.datetime}`);
 
-          return(filterValue === sighting.datetime);
+          filtered = (filterValue === sighting.datetime);
           break;
         case "city":
           // code block
           console.log(`${filterValue} compared to ${sighting.city}`);
-          return(filterValue === sighting.city)
+          filtered = (filterValue === sighting.city)
           break;
+        case "state":
+            // code block
+            console.log(`${filterValue} compared to ${sighting.state}`);
+            filtered = (filterValue === sighting.state)
+            break;
+        case "country":
+            // code block
+            console.log(`${filterValue} compared to ${sighting.country}`);
+            filtered = (filterValue === sighting.country)
+            break;
         default:
+            //should never reach here
+            console.log(`${filterName}: ${filterValue} compared to ${sighting.filterName}`);
           // code block
       }
-
-
-
-}
-
-function filterTable2(sighting) {
-    //var x = document.getElementByName("datetime");
-    // var inputElement = d3.select("#text");
-
-    console.log(`Filter Value: ${filterValue}`);
-    console.log(`Filter Field: ${filterField}`)
-
-    if(filterField === 'datetime'){
-        console.log("DATETIME");
-        var inputElement = d3.selectAll(".form-control").filter(function(d, i) { return i === 0}); 
-        var inputValue = inputElement.property("value");
-        var inputName  = inputElement.property("name");
-    
-        console.log(`TARGET ${inputValue}`);
-        console.log(`FIELD ${inputName}`);
-    
-        console.log(`${inputValue} compared to ${sighting.datetime}`);
-    
-        return(inputValue === sighting.datetime)
-    }
-
-    if(filterField === 'city'){
-        console.log("CITY");
-        var inputElement = d3.selectAll(".form-control").filter(function(d, i) { return i === 1}); 
-        var inputValue = inputElement.property("value");
-        var inputName  = inputElement.property("name");
-    
-        console.log(`TARGET ${inputValue}`);
-        console.log(`FIELD ${inputName}`);
-    
-    
-        console.log(`${inputValue} compared to ${sighting.city}`);
-    
-        return(inputValue === sighting.city)
-    }
-    //var inputElement = d3.selectAll(".form-control").filter(function(d, i) { return i === 1}); 
-
-    //console.log(inputElement.property.length);
-
-    // var inputValue = inputElement.property("value");
-    // var inputName  = inputElement.property("name");
-
-    // console.log(`TARGET ${inputValue}`);
-    // console.log(`FIELD ${inputName}`);
-
-
-    // console.log(`${inputValue} compared to ${sighting.city}`);
-
-    // return(inputValue === sighting.city)
+      return(filtered);
 }
 
 function printTable(results){
@@ -161,26 +83,19 @@ function printTable(results){
     });
 }
 
-function loadAll (){
-    createTable(data)
-}
-
 var loadAllButton = d3.select("#loadall-btn");
 loadAllButton.on("click", function(){
     var tbody = d3.select("tbody");
 
     tbody.selectAll('tr').remove();
 
-    //console.log(`LOAD ALL TBODY ${tbody}`);
-
     createTable(data);
     //console.log(data)
 });
 
-var button = d3.select("#filter-btn");
 
 button.on("click", function (){
-    var results = data.filter(filterTable);
+    var results = data.filter(filterData);
 
     var tbody = d3.select("tbody");
     tbody.selectAll('tr').remove();
@@ -196,18 +111,8 @@ function handleChange(event){
     console.log(`Handle Change Value: ${filterValue}`);
     console.log(`Handle Change Value: ${filterName}`)
 
-    // var inputElement = d3.selectAll(".form-control")
-    // var inputName  = inputElement.property("name");
-    // console.log(inputName);
-    console.log(this.name);
-    console.log(this.value)
-
-    // if(filterField === "datetime"){
-    //     var results = data.filter(filterDate);
-    // }
-    // if(filterField === "city"){
-    //     var results = data.filter(filterCity);
-    // }
+    // console.log(this.name);
+    // console.log(this.value)
 
     var results = data.filter(filterData)
 
